0614c6f82fab3231f7e0a2715eb44cc31535c581
Yurii <pamkivurij92@gmail.com>
2025-12-14 13:48:05 +0100

    Solution #2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 class Animal:
-    alive = []
+    alive: list["Animal"] = []
 
-    def __init__(self, name: str, health: int = 100) -> None:
+    def __init__(
+            self,
+            name: str,
+            health: int = 100
+    ) -> None:
         self.health = health
         self.name = name
         self.hidden = False
@@ -22,9 +26,8 @@ class Herbivore(Animal):
 
 class Carnivore(Animal):
     @staticmethod
-    def bite(herb: Herbivore) -> None:
+    def bite(herb: Animal) -> None:
         if not (isinstance(herb, Carnivore) or herb.hidden):
             herb.health -= 50
-
-        if herb.health <= 0:
-            Animal.alive.remove(herb)
+            if herb.health <= 0:
+                Animal.alive.remove(herb)
